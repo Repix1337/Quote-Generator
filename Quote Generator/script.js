f0cce2a17764add09a4824e7ce10d4380564ddd1
Repix1337 <91416941+Repix1337@users.noreply.github.com>
2024-01-19 21:15:11 +0100

    Add files via upload

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -24,5 +24,62 @@ fetch('data.json')
       document.getElementById("quote").textContent = quote;
     })
     
+    batman.addEventListener("mouseover", () => {
+      let obraz = document.createElement('img');
+      obraz.src = "pobrane.jpg";
+      obraz.style.position = "fixed";
+      obraz.style.top = "50%"; /* Adjust the positioning as needed */
+      obraz.style.right = "60%";
+      obraz.style.transform = "translate(-50%, -50%)";
+      obraz.style.transform = "scale(0.3)";
+      obraz.style.zIndex = "2";
+      obraz.alt = 'Alternative Text';
+      batman.appendChild(obraz);
+      batman.addEventListener("mouseout", () => {
+        obraz.style.transition = 'opacity 0.5s ease';
+        obraz.style.opacity = '0';
+        setTimeout(() => {
+          obraz.remove();
+        }, 600);
+      })
+      joker.addEventListener("mouseover", () => {
+        let obraz = document.createElement('img');
+        obraz.src = "pobrane.jpg";
+        obraz.style.position = "fixed";
+        obraz.style.top = "50%"; /* Adjust the positioning as needed */
+        obraz.style.right = "60%";
+        obraz.style.transform = "translate(-50%, -50%)";
+        obraz.style.transform = "scale(0.3)";
+        obraz.style.zIndex = "2";
+        obraz.alt = 'Alternative Text';
+        joker.appendChild(obraz);
+        joker.addEventListener("mouseout", () => {
+          obraz.style.transition = 'opacity 0.5s ease';
+          obraz.style.opacity = '0';
+          setTimeout(() => {
+            obraz.remove();
+          }, 600);
+        })
+        frank.addEventListener("mouseover", () => {
+          let obraz = document.createElement('img');
+          obraz.src = "pobrane.jpg";
+          obraz.style.position = "fixed";
+          obraz.style.top = "50%"; /* Adjust the positioning as needed */
+          obraz.style.right = "60%";
+          obraz.style.transform = "translate(-50%, -50%)";
+          obraz.style.transform = "scale(0.3)";
+          obraz.style.zIndex = "2";
+          obraz.alt = 'Alternative Text';
+          frank.appendChild(obraz);
+          frank.addEventListener("mouseout", () => {
+            obraz.style.transition = 'opacity 0.5s ease';
+            obraz.style.opacity = '0';
+            setTimeout(() => {
+              obraz.remove();
+            }, 600);
+          })
+
+        })
     
-  })
+    
+      })})})
