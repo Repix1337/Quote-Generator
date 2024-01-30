90accc065326e75da8aca988795c751a68bd231a
Repix1337 <91416941+Repix1337@users.noreply.github.com>
2024-01-30 12:46:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,129 +1,90 @@
-let batman = document.getElementById("batman")
-let joker = document.getElementById("joker")
-let frank = document.getElementById("frank")
+let characters = {
+  batman: {
+    element: document.getElementById("batman"),
+    image: "batman.jpg",
+    imageBackground: "batmanBackground.jpg",
+    text: "Batman once said...",
+  },
+  joker: {
+    element: document.getElementById("joker"),
+    image: "joker.jpg",
+    imageBackground: "jokerBackground.jpg",
+    text: "Joker once said...",
+  },
+  frank: {
+    element: document.getElementById("frank"),
+    image: "frank.jpg",
+    imageBackground: "frankBackground.jpg",
+    text: "Frank Sinatra once said...",
+  },
+  kanye: {
+    element: document.getElementById("kanye"),
+    image: "kanye.jpg",
+    imageBackground: "kanyeBackground.jpg",
+    text: "Kanye West once said...",
+  },
+  pilsudski: {
+    element: document.getElementById("pilsudski"),
+    image: "Pilsudski.jpg",
+    imageBackground: "pilsudskiBackground.jpg",
+    text: "Józef Piłsudski once said...",
+  },
+};
+
 let character = document.querySelector("h2");
-let quoteText =document.getElementById("quote")
-fetch('data.json')  
+let quoteText = document.querySelector("h3");
+
+fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    batman.addEventListener("click", () => {
-      if (character.textContent != "Batman once said...")
-      {
-      character.style.transition = 'opacity 0.5s ease';
-      character.style.opacity = '0';
-      setTimeout(() => {
-      character.style.transition = 'opacity 0.5s ease';
-      character.style.opacity = '1';
-      }, 500);
-    }
-    quoteText.style.transition = 'opacity 0.5s ease';
-    quoteText.style.opacity = '0';
-    let id = Math.floor(Math.random() * 15) + 1;
-    let quote = data["Batman"][id].quote;
-    setTimeout(() => {
-      quoteText.style.transition = 'opacity 0.5s ease';
-      quoteText.style.opacity = '1';
-      character.textContent = "Batman once said..."
-      quoteText.textContent = quote;
-      }, 500);
-    })
-    joker.addEventListener("click", () => {
-      if (character.textContent != "Joker once said...")
-      {
-      character.style.transition = 'opacity 0.5s ease';
-      character.style.opacity = '0';
-      setTimeout(() => {
-      character.style.transition = 'opacity 0.5s ease';
-      character.style.opacity = '1';
-      }, 500);
-    }
-    quoteText.style.transition = 'opacity 0.5s ease';
-    quoteText.style.opacity = '0';
-    let id = Math.floor(Math.random() * 15) + 1;
-    let quote = data["Joker"][id].quote;
-    setTimeout(() => {
-      quoteText.style.transition = 'opacity 0.5s ease';
-      quoteText.style.opacity = '1';
-      character.textContent = "Joker once said..."
-      quoteText.textContent = quote;
-      }, 500);
-    })
-    
-    frank.addEventListener("click", () => {
-      if (character.textContent != "Frank Sinatra once said...")
-      {
-        character.style.transition = 'opacity 0.5s ease';
-        character.style.opacity = '0';
+    for (const key in characters) {
+      characters[key].element.addEventListener("click", () => {
+        if (character.textContent != characters[key].text) {
+          character.style.transition = 'opacity 0.5s ease';
+          character.style.opacity = '0';
+          setTimeout(() => {
+            character.style.transition = 'opacity 0.5s ease';
+            character.style.opacity = '1';
+          }, 500);
+        }
+
+        quoteText.style.transition = 'opacity 0.5s ease';
+        quoteText.style.opacity = '0';
+        let id = Math.floor(Math.random() * 40) + 1;
+        let quote = data[key][id].quote;
+
         setTimeout(() => {
-        character.style.transition = 'opacity 0.5s ease';
-        character.style.opacity = '1';
+          quoteText.style.transition = 'opacity 0.5s ease';
+          quoteText.style.opacity = '1';
+          character.textContent = characters[key].text;
+          quoteText.textContent = quote;
         }, 500);
-      }
-    quoteText.style.transition = 'opacity 0.5s ease';
-    quoteText.style.opacity = '0';
-    let id = Math.floor(Math.random() * 15) + 1;
-    let quote = data["Frank"][id].quote;
-    setTimeout(() => {
-      quoteText.style.transition = 'opacity 0.5s ease';
-      quoteText.style.opacity = '1';
-      character.textContent = "Frank Sinatra once said..."
-      quoteText.textContent = quote;
-      }, 500);
-    })
-  })
-  let batmanImage = document.createElement('img');
-batmanImage.src = "batman.jpg"; 
-batmanImage.style.position = "absolute";
-batmanImage.style.width = "150px";
-batmanImage.style.height = "auto";
-batmanImage.style.zIndex = "2";
-batmanImage.style.opacity = '0';
-batman.appendChild(batmanImage); 
+        document.body.style.backgroundImage = 'url(' + characters[key].imageBackground + ')';
+        document.body.style.backgroundRepeat = "no-repeat"
+      });
+    }
+  });
 
-batman.addEventListener("mouseover", () => {
-    batmanImage.style.transition = 'opacity 0.5s ease';
-    batmanImage.style.opacity = '1';
-});
+for (const key in characters) {
+  let characterImage = document.createElement('img');
+  characterImage.src = characters[key].image;
+  characterImage.style.position = "absolute";
+  characterImage.style.width = "150px";
+  characterImage.style.height = "auto";
+  characterImage.style.opacity = '0';
+  characters[key].element.appendChild(characterImage);
 
-batman.addEventListener("mouseout", () => {
-    batmanImage.style.transition = 'opacity 0.5s ease';
-    batmanImage.style.opacity = '0';
-});
+  characters[key].element.addEventListener("mouseover", () => {
+    characterImage.style.transition = 'opacity 0.5s ease';
+    characterImage.style.opacity = '1';
+    characters[key].element.text.style.transition = 'opacity 0.5s ease';
+    characters[key].element.text.style.opacity = '0';
+  });
 
-let jokerImage = document.createElement('img');
-jokerImage.src = "joker.jpg"; 
-jokerImage.style.position = "absolute";
-jokerImage.style.width = "150px";
-jokerImage.style.height = "auto";
-jokerImage.style.zIndex = "2";
-jokerImage.style.opacity = '0';
-joker.appendChild(jokerImage); 
-
-joker.addEventListener("mouseover", () => {
-    jokerImage.style.transition = 'opacity 0.5s ease';
-    jokerImage.style.opacity = '1';
-});
-
-joker.addEventListener("mouseout", () => {
-    jokerImage.style.transition = 'opacity 0.5s ease';
-    jokerImage.style.opacity = '0';
-});
-
-let frankImage = document.createElement('img');
-frankImage.src = "frank.jpg";
-frankImage.style.position = "absolute";
-frankImage.style.width = "150px";
-frankImage.style.height = "auto";
-frankImage.style.zIndex = "2";
-frankImage.style.opacity = '0';
-frank.appendChild(frankImage); 
-
-frank.addEventListener("mouseover", () => {
-    frankImage.style.transition = 'opacity 0.5s ease';
-    frankImage.style.opacity = '1';
-});
-
-frank.addEventListener("mouseout", () => {
-    frankImage.style.transition = 'opacity 0.5s ease';
-    frankImage.style.opacity = '0';
-});
+  characters[key].element.addEventListener("mouseout", () => {
+    characterImage.style.transition = 'opacity 0.5s ease';
+    characterImage.style.opacity = '0';
+    characters[key].element.text.style.transition = 'opacity 0.5s ease';
+    characters[key].element.text.style.opacity = '1';
+  });
+}
